249a8961fc9724558091c526f709e0d956a4bf13
lucio rendine <luciorendine69@gmail.com>
2021-04-15 15:21:37 +0200

    vue search

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+new Vue({
+  el: '#app',
+  data: {
+    results:[],
+    restaurantList:[],
+    userSearch:'',
+  },
+  methods:{
+    search() {
+      this.results = [];
+      this.searchrestaurant();
+    },
+    searchrestaurant() {
+      const self = this;
+      axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=0071f8ee5cbfa7dc991e243699c56675&query=' + self.userSearch)
+        .then(function(result) {
+          const restaurantList = result.data.results;
+
+          self.restaurantList = restaurantList;
+          self.results = [...self.restaurantList,...self.results]
+          self.userSearch = '';
+
+        });
+    }
+  },
+
+})
+Vue.config.devtools = true
